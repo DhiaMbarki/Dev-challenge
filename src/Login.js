9ca7b77bb82efdebddb5f304ea057eee6fb597ac
DhiaMbarki <tahridhia22@gmail.com>
2021-02-25 20:43:15 +0100

    switch login, signup on the same component

--- src/Login.js
+++ src/Login.js
@@ -16,17 +16,39 @@ const Login = (props) => {
   return (
     <section className="login">
       <div className="loginContainer">
-      <label>UserName</label>
-      <input
-        type="text "
-        autoFocus
-        required
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <p className = "errorMsj">{emailError}</p>
-      <label>password</label>
-      <input type ="password" required value ={password} onChange ={e => setPassword(e.target.value) }/>
+        <label>UserName</label>
+        <input
+          type="text "
+          autoFocus
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <p className="errorMsj">{emailError}</p>
+        <label>password</label>
+        <input
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p className = "errorMsj">{passwordError}</p>
+        <div className = "btnContainer">
+          {hasAccount ? (
+            <>
+
+            <button>Sign in</button>
+            <p>I dont have account ? <span>sign Up</span> </p> 
+            </>
+
+          ) : (
+            <>
+            <button>sign up</button>
+            <p>I already have account ? <span>Sign in</span> </p>
+            </>
+
+          )} 
+        </div>
       </div>
     </section>
   );
