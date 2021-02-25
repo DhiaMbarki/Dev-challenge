9da9e850c983cb8c8c5dd03eeb684639891bf0a8
DhiaMbarki <tahridhia22@gmail.com>
2021-02-25 13:21:44 +0100

    first step

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import React from "react";
+import Firebase from './Firebase'
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [hasAccount, setHasAccount] = useState("false");
+  
   return (
     <div className="App">
-     <h1>Ne9es</h1>
+      <h1>Ne9es</h1>
     </div>
   );
 }
